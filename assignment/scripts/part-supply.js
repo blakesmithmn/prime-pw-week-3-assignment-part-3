766cc63810d16abaa1bd0cad716bb0f3051e29f1
Blake Smith <blake.smith.mn@gmail.com>
2022-08-03 18:17:01 -0500

    added 'let' to the for loops for my sake

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -35,7 +35,7 @@ console.log(supplyChanges);
 //    - if the value is 0, log 'No Change.'
 //    - if the value is negative, format the log as 'Removed x parts.' 
 console.log('6. Showing supplyChanges...');
-for (i = 0; i <= supplyChanges.length; i++) {
+for (let i = 0; i < supplyChanges.length; i++) {
     if (supplyChanges[i] > 0) {
         console.log('Added', supplyChanges[i], 'parts');
     }
